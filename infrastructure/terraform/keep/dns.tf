ac79b7720f3fdc72bf5fcc614418fde1a176b236
Sloan Thompson <sloan.thompson@thesis.co>
2020-05-20 13:02:19 -0400

    Use resource output for CNAME value + ecdsa
    Adding first keep-ecdsa-client entry and shifting all entries resource
    lookups for ELB hostname, instead of hardcoding.

--- infrastructure/terraform/keep/dns.tf
+++ infrastructure/terraform/keep/dns.tf
@@ -11,7 +11,7 @@ resource "aws_route53_record" "keep-beacon-client-0" {
   name    = "keep-beacon-client-0.${data.aws_route53_zone.mazeltov.name}"
   type    = "CNAME"
   ttl     = "300"
-  records = ["a22e8be63a1164cd190e4068e76fab09-1380995297.ca-central-1.elb.amazonaws.com"]
+  records = [kubernetes_service.keep-beacon-client-0.load_balancer_ingress["0"].hostname]
 }
 
 
@@ -20,5 +20,13 @@ resource "aws_route53_record" "keep-beacon-client-1" {
   name    = "keep-beacon-client-1.${data.aws_route53_zone.mazeltov.name}"
   type    = "CNAME"
   ttl     = "300"
-  records = ["ad3b2e544248a4efbb5027e2426c27a4-1248079851.ca-central-1.elb.amazonaws.com"]
+  records = [kubernetes_service.keep-beacon-client-1.load_balancer_ingress["0"].hostname]
+}
+
+resource "aws_route53_record" "keep-ecdsa-client-0" {
+  zone_id = data.aws_route53_zone.mazeltov.zone_id
+  name    = "keep-ecdsa-client-0.${data.aws_route53_zone.mazeltov.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [kubernetes_service.keep-ecdsa-client-0.load_balancer_ingress["0"].hostname]
 }
